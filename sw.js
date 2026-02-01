0a3cc5218524e48d29d8fb9cf62bb10bb1503ec3
Yash Srivastava <srivastavayash100@gmail.com>
2026-02-01 05:37:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,31 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Service Worker: Registered'))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
-  });
-}
+const CACHE_NAME = 'smart-spend-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Caching Assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activated');
+});
+
+// Fetch Assets from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
