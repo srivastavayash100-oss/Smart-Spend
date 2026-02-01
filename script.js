da9ff490e813e19fb24f593954d8d3a7ab82902f
Yash Srivastava <srivastavayash100@gmail.com>
2026-02-01 12:18:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1147,7 +1147,7 @@ async function fullResetAllData() {
 }
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(reg => console.log('SW Registered!'))
       .catch(err => console.log('SW Registration Failed: ', err));
   });
