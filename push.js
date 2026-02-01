07d70fbdc85a52cfdddeccf01e66b1855d1bb730
Yash Srivastava <srivastavayash100@gmail.com>
2026-02-01 23:32:25 +0530

    Update push.js

--- push.js
+++ push.js
@@ -1,3 +1,18 @@
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 async function enablePush() {
   if (!('serviceWorker' in navigator) || !('PushManager' in window)) {
     alert('Push not supported on this device/browser yet.');
@@ -11,7 +26,9 @@ async function enablePush() {
 
   const sub = await reg.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: 'BEz-RQJ9Krggi13aFiu2veQEuqNvDbkKn7CrJgPN6MBPJib2oU2jOW8Z6x28dSoegUe0GrOJmkj-UpcB9POtpvU'
+    applicationServerKey: urlBase64ToUint8Array(
+      'BEz-RQJ9Krggi13aFiu2veQEuqNvDbkKn7CrJgPN6MBPJib2oU2jOW8Z6x28dSoegUe0GrOJmkj-UpcB9POtpvU'
+    )
   });
 
   console.log('PUSH SUBSCRIPTION:', JSON.stringify(sub));
